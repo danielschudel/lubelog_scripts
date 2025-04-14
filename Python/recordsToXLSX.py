b2adfd5f3812e42923f42113f8f2cf729ba10bf0
Daniel Schudel <dan.schudel@gmail.com>
2025-04-14 18:45:12 -0400

    first pass at xlsx

--- Python/recordsToXLSX.py
+++ Python/recordsToXLSX.py
@@ -3,6 +3,10 @@
 import requests
 import json
 import sys
+import xlsxwriter
+import datetime
+from datetime import datetime
+
 
 urlBase = sys.argv[1]
 urlUpgrades = urlBase + "/api/vehicle/upgraderecords" + "?vehicleId="
@@ -31,10 +35,7 @@ for v in vehicles:
                 current[key] = r[key]
 
             current['odometer'] = int(r['odometer'])
-            if r['cost'] != '0':
-                current['cost']     = "${:.02f}".format(float(r['cost']))
-            else:
-                current['cost']     = ""
+            current['cost']     = float(r['cost'])
 
             if current['notes'] == None:
                 current['notes'] = ""
@@ -42,13 +43,41 @@ for v in vehicles:
             combined.append(current)
 
     # sorted_list = sorted(my_list, key=lambda x: x['age'])
-    filename="{}.tsv".format(identifier)
+    filename="{}.xlsx".format(identifier)
+
+    # Create a workbook and add a worksheet.
+    workbook = xlsxwriter.Workbook(filename)
+    formatCost     = workbook.add_format({'num_format': '$#,##0', 'align': 'right'})
+    formatOdometer = workbook.add_format({'num_format':  '#,##0', 'align': 'right'})
+    formatDate     = workbook.add_format({'num_format': 'mm-dd-yyyy', 'align': 'right'})
+    formatBold     = workbook.add_format({'bold': True})
+    formatWrap     = workbook.add_format({'text_wrap': True})
+    formatTop      = workbook.add_format({'valign': 'top'})
+
+
+    worksheet = workbook.add_worksheet()
+
+    row=0
+    col=0
+    for key in [ "Date", "Odometer", "Description", "Notes", "Cost" ]:
+        worksheet.write(row, col, key, formatBold)
+        col+=1
+
+    for r in sorted(combined, key=lambda x: x['odometer']):
+        row+=1
+        worksheet.write_datetime(row, 0, datetime.strptime(r['date'], "%m/%d/%Y"))
+        worksheet.write_number(row, 1, r["odometer"])
+        worksheet.write_string(row, 2, r["description"])
+        worksheet.write_string(row, 3, r["notes"])
+        if r['cost'] == 0:
+            worksheet.write_blank(row, 4, None)
+        else:
+            worksheet.write_number(row, 4, r["cost"])
+
+    worksheet.set_column('A:A', 12, formatDate)
+    worksheet.set_column('B:B', 10, formatOdometer)
+    worksheet.set_column('C:C', 25)
+    worksheet.set_column('D:D', 35, formatWrap)
+    worksheet.set_column('E:E', 10, formatCost)
+    workbook.close()
     print("             Saved: {}".format(filename))
-    with open("{}.tsv".format(identifier), "w") as f:
-        f.write("Date\tOdometer\tDescription\tNotes\tCost\n")
-        for r in sorted(combined, key=lambda x: x['odometer']):
-            f.write('{}\t'.format(r['date']))
-            f.write('"{:,}"\t'.format(r['odometer']))
-            f.write('"{}"\t'.format(r['description']))
-            f.write('"{}"\t'.format(r['notes']))
-            f.write('{}\n'.format(r['cost']))
